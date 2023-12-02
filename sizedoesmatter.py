18d3f47dad5fa89e919c7b06ece0fffc38662f9b
ndbellew <ndbellew@gmail.com>
2023-12-02 09:31:51 -0700

    hh

--- sizedoesmatter.py
+++ sizedoesmatter.py
@@ -1,5 +1,6 @@
 def calcNE(p,k):
     ans = p + k*p**2
+    #print(ans)
     return ans
 
 n = 48789828618640905340268650455641742611513932500406667
@@ -7,10 +8,14 @@ e = 65537
 encMessage = 35527844866852571287965978399657369491580791928209728
 factorsOfN = [176795785843333659350390219, 275967146987743635463977793]
 p,q = factorsOfN
-
-for i in range(e):
+PHI = (p-1) * (q-1)
+d = (e**-1) % PHI
+print(d)
+for i in range(1,e):
     if calcNE(p,i) == n*e:
         K = i
         break
-
-print(K)
+try:
+    print(K)
+except NameError as namerror:
+    print(n*e)
